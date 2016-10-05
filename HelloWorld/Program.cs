492dc51c65830a509e5e37730285b324eb690a92
vitalijusz <vitalijusz@gmail.com>
2016-10-05 13:24:18 +0300

    First

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        protected static void Main(string[] args)
+        {
+            Console.writeline("Hello World");
+            Console.ReadLine();
+        }
+    }
+}

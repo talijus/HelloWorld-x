1cad7f2548863b8fc2acc04d14aba9caff734ff1
vitalijusz <vitalijusz@gmail.com>
2016-10-06 15:28:28 +0300

    HomeWork 2.2

--- HelloWorld/HomeWork.cs
+++ HelloWorld/HomeWork.cs
@@ -1,4 +1,6 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     public class HomeWork
     {
@@ -57,10 +59,103 @@
 
     public class HomeWorkV2 : HomeWork
     {
-        public string QQ()
+
+        public string MaxSymbol(string word)
         {
-            return "QQ";
+            SymbolHistogram wsd = new SymbolHistogram();
+
+            wsd.LoadHistogram(word);
+
+
+            return wsd.Result();
         }
+    }
+
+
+
+
+    public class SymbolHistogram
+    {
+        public struct Histogram
+        {
+            public char symbol;
+            public int frequency;
+
+            
+            public Histogram(int i, char c)
+            {
+                symbol = c;
+                frequency = i;
+            }
+            /**/
+        }
+
+        public Histogram[] hist;
+
+        public void LoadHistogram(string word)
+        {
+            int i;
+            int j;
+            Array.Resize(ref hist, 1);
+
+            for (i = 0; i < word.Length; i++)
+            {
+                j = Exist(word[i]);
+                if (j == -1)
+                {
+                    AddSymbol(word[i]);
+                }
+                else
+                {
+                    UpdateSymbol(j);
+                }
+            }
+            Array.Resize(ref hist, hist.Length - 1);
+        }
+
+        private void AddSymbol (char ss)
+        {
+            hist[hist.Length-1].symbol = ss;
+            hist[hist.Length-1].frequency = 1;
+            Array.Resize(ref hist, hist.Length + 1);
+        }
+
+        private void UpdateSymbol(int i)
+        {
+            hist[i].frequency = hist[i].frequency + 1;
+        }
+
+        private int Exist(char ss)
+        {
+            int i;
+
+            for (i=0; i < hist.Length; i++)
+            {
+                if (string.Compare(ss.ToString(), hist[i].symbol.ToString()) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+        public string Result()
+        {
+            int max = 0;
+            int i;
+
+            for (i = 0; i < hist.Length; i++)
+            {
+                if (hist[i].frequency > hist[max].frequency)
+                    max = i;
+
+            }
+            //Console.WriteLine(hist[max].symbol.ToString() + " - " + hist[max].frequency.ToString());
+
+            return hist[max].symbol.ToString() + " - " + hist[max].frequency.ToString();
+        }
+
     }
 }
 
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,24 +7,39 @@ namespace HelloWorld
     {
         protected static void Main(string[] args)
         {
+            string ar="";
             if (args.Length < 1)
             {
                 Console.WriteLine("No arguments, program requires at least one argument");
                 return;
             }
 
-            /*var module = new HelloModule();
+            /*
+             * var module = new HelloModule();
             Console.WriteLine(module.SayHello(args[0]));
-            Console.ReadLine();*/
-            Console.WriteLine("Starting test...");
-            Console.WriteLine(args[0]);
+            Console.ReadLine();
+            */
+            Console.WriteLine("Starting test...\n");
+
+            Console.WriteLine("Arguments:");
+            for (int i = 0; i < args.Length; i++)
+            {
+                ar = ar + args[i] + " ";
+            }
+            ar = ar.TrimEnd();
+                
+            Console.WriteLine(ar + "\n");
+
+            Console.WriteLine("HomeWork 1\n");
 
             var HomeWorkObj1 = new HelloWorld.HomeWork();
             Console.WriteLine(HomeWorkObj1.SentenceCase(args));
 
-            
 
+            Console.WriteLine("\nHomeWork 2\n");
             var HomoWorkObj2 = new HelloWorld.HomeWorkV2();
+
+            Console.WriteLine("most frequently used symbol is : " + HomoWorkObj2.MaxSymbol(ar));
 
             Console.ReadLine();
         }
